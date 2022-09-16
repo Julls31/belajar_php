b69f2c2bbb0aabcc6fa9ffc3ad58e25bcd7ce4f7
Julls31 <jullyandrefazry@gmail.com>
2022-09-16 17:29:57 +0700

    belajar string dan array dikit diperbaiki

--- index.php
+++ index.php
@@ -1,23 +1,28 @@
 
 <?php 
-	$nama1 = "Jullyandre";
-	$nama2 = "JullyandrE";
-	$output = strcmp($nama1, $nama2);
+	// $nama1 = "Jullyandre";
+	// $nama2 = "JullyandrE";
+	// $output = strcmp($nama1, $nama2);
 	
-		// echo $nama1 . "<br>"; 
-		// // echo "<br>";
-		// echo ($nama2) . "<br>";
-		// echo strlen($nama1);
-		// echo $output;
-		echo strtoupper($nama1) . "<br>";
-		echo strtolower($nama2) . "<br>";
+	// 	// echo $nama1 . "<br>"; 
+	// 	// echo "<br>";
+	// 	// echo ($nama2) . "<br>";
+	// 	// echo strlen($nama1);
+	// 	// echo $output;
+	// 	echo strtoupper($nama1) . "<br>";
+	// 	echo strtolower($nama2) . "<br>";
 
-		if ($output !== 4) {
-			echo "Namanya salah";
-		}
-			else {
-				echo "Namanya benar";
-			}
+	// 	if ($output !== 4) {
+	// 		echo "Namanya salah";
+	// 	}
+	// 		else {
+	// 			echo "Namanya benar";
+	// 		}
+
+ 	$nama3 = "Saya suka PHP";
+ 	$nama4 = "A";
+
+ 	
 
 
 
