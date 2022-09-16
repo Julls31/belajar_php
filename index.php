f9c5446a7bc2ea38a49e2b0b3c52d179cdf6969a
Julls31 <jullyandrefazry@gmail.com>
2022-09-16 17:26:54 +0700

    belajar string dan array dikit

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<?php 
+	$nama1 = "Jullyandre";
+	$nama2 = "JullyandrE";
+	$output = strcmp($nama1, $nama2);
+	
+		// echo $nama1 . "<br>"; 
+		// // echo "<br>";
+		// echo ($nama2) . "<br>";
+		// echo strlen($nama1);
+		// echo $output;
+		echo strtoupper($nama1) . "<br>";
+		echo strtolower($nama2) . "<br>";
+
+		if ($output !== 4) {
+			echo "Namanya salah";
+		}
+			else {
+				echo "Namanya benar";
+			}
+
+
+
+ ?>
