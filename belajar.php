f47ac98f22e50b3cbe623805acea4f5cc8f7be3c
Julls31 <jullyandrefazry@gmail.com>
2022-09-20 16:47:31 +0700

    belajar tanggal 20 sept

---
+++ belajar.php
@@ -0,0 +1,83 @@
+<?php 
+
+// $panjang = 12;
+// $lebar = 3;
+
+// $luas = $panjang * $lebar;
+
+// echo $luas;
+
+////////////////////////////////////
+
+// $tambahdua = 2;
+// $tambahdua += 2;
+
+// echo $tambahdua;
+
+///////////////////////////////////
+
+// $angka = 10;
+
+// echo $angka == 10;
+// var_dump($angka ==10);
+
+//////////////////////////////////
+
+// $sifat = "rajin";
+
+// var_dump($sifat != "malas");
+
+/////////////////////////////////
+
+// var_dump(false && false);
+// var_dump(true || false);
+
+// $angka1 = 25;
+// $angka2 = 45;
+
+// if ($angka1 == 25 || $angka2 == 45) {
+// 	echo " Kondisi benar";
+// }
+// else {
+// 	echo "Kondisi salah";
+// }
+
+//////////////////////////////////
+
+// function garisbaru()
+// {
+// 	echo "asdafajf";
+
+// }
+
+// garisbaru("");
+
+
+// function  buat_nama_kapital($nama)
+// 	{
+// 		return ucwords($nama);
+
+// 	}
+
+// $nama_lengkap = buat_nama_kapital("jullyandre fazri");
+
+// echo $nama_lengkap;
+
+/////////////////////////////////////////
+
+// for ($i=5; $i >= 1; $i--) {
+// 	echo "ini adalah looping ke $i<br>";
+//  }
+
+$x = 5;
+
+while ($x <= 10) {
+	echo "$x - lima sampai sepuluh <br>";
+	$x++;
+}
+while ($x <= 10) {
+
+}
+
+
+ ?>
